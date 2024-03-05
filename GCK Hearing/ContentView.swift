cc883c0c85ff4e776e3dbaed09a490bd1e329270
sharrarwasit <sharrar.wasit@gocheckkids.com>
2024-03-05 13:14:48 -0500

    Initial Commit

---
+++ GCK Hearing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GCK Hearing
+//
+//  Created by Sharrar Wasit on 3/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GCK Hearing/GCK_HearingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCK_HearingApp.swift
+//  GCK Hearing
+//
+//  Created by Sharrar Wasit on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GCK_HearingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
