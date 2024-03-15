0f8a512f4b165b43bd2ddd63bb35296067a330ae
sharrarwasit <sharrar.wasit@gocheckkids.com>
2024-03-15 17:02:41 -0400

    updated navigation to use UIKit Coordinator pattern

--- GCK Hearing/Features/PatientList/Views/PatientListView.swift
+++ GCK Hearing/Features/PatientList/Views/PatientListView.swift
@@ -16,6 +16,7 @@ struct PatientListScreen: View {
     
     var body: some View {
         
+        NavigationView {
             VStack {
                 //                SearchBarView(searchText: $vm.searchText, placeholderText: "Search for a patient ...")
                 //                Spacer()
@@ -25,36 +26,52 @@ struct PatientListScreen: View {
                     case .loading:
                         PatientListLoadingView()
                     case .success:
-                        PatientListLoadedView(patientList: vm.allPatients, dateString: "02/11/2024", action: { name in
-                            
-                                 vm.goToDetails(name)
-                            
+                        PatientListLoadedView(patientList: vm.allPatients, dateString: "02/11/2024", detailAction: { patient in
+                            vm.userTapAction?(patient)
+                        }, screenAction: { patient in
+                            vm.screenTapAction?(patient)
                         })
-//                        PatientListLoadedView(patientList: vm.allPatients, dateString: "02/07/2024", action: vm.goToDetails(_:))
-//                        PatientListLoadedView(patientList: vm.allPatients, dateString: "02/03/2024", action: vm.goToDetails(_:))
+                        PatientListLoadedView(patientList: vm.allPatients, dateString: "02/05/2024", detailAction: { patient in
+                            vm.userTapAction?(patient)
+                        }, screenAction: { patient in
+                            vm.screenTapAction?(patient)
+                        })
+                        PatientListLoadedView(patientList: vm.allPatients, dateString: "01/29/2024", detailAction: { patient in
+                            vm.userTapAction?(patient)
+                        }, screenAction: { patient in
+                            vm.screenTapAction?(patient)
+                        })
+                        PatientListLoadedView(patientList: vm.allPatients, dateString: "01/25/2024", detailAction: { patient in
+                            vm.userTapAction?(patient)
+                        }, screenAction: { patient in
+                            vm.screenTapAction?(patient)
+                        })
                     case .failed:
                         PatientListErrorView()
                     }
                 }
+                .searchable(text: $vm.searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Find patient by name or ID")
                 .listStyle(.plain)
             }
             .navigationTitle("Patients")
             .navigationBarTitleDisplayMode(.inline)
-//            .toolbar {
-//                ToolbarItem(placement: .topBarLeading) {
-//                    RoundedButton(title: "Menu", color: .cyan, action: {})
-//                }
-//                ToolbarItem(placement: .topBarTrailing) {
-//                    RoundedButton(title: "Add", color: .cyan, action: {})
-//                }
-//            }
-        
-        .onAppear {
-            Task {
-                await vm.loadPatients()
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    RoundedButton(title: "Menu", color: Color.deepBlueTheme.accentDark, action: {})
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    RoundedButton(title: "Add", color: Color.deepBlueTheme.accentDark, action: {})
+                }
+            }
+            
+            .onAppear {
+                Task {
+                    await vm.loadPatients()
+                }
             }
         }
-//        .searchable(text: $vm.searchText, prompt: "Find patient by name or ID")
+        
     }
 }
 
@@ -62,21 +79,34 @@ extension PatientListScreen {
     struct PatientListLoadingView: View {
         var body: some View {
             ProgressView()
+                .frame(maxWidth: .infinity)
+                .padding()
         }
     }
     
     struct PatientListLoadedView: View {
         let patientList: [Patient]
         let dateString: String
-        let action: (Patient) -> Void
+        let detailAction: (Patient) -> Void
+        let screenAction: (Patient) -> Void
+        
         var body: some View {
             Section {
-                ForEach(patientList) { patient in
-                    PatientRow(patientName: patient.fullName)
-                        .onNavigation {
-                            action(patient)
+                    ForEach(patientList) { patient in
+                        HStack {
+                            PatientRow(patientName: patient.fullName)
+                                .onTapGesture {
+                                    detailAction(patient)
+                                }
+                            Spacer()
+                            RoundedButton(title: "Screen", color: .theme.accent) {
+                                // screenaction
+                                screenAction(patient)
+                            }
                         }
-                }
+                    }
+                   
+                
                 .listRowInsets(.init(top: 0, leading: 10, bottom: 5, trailing: 10))
                 .listRowSeparator(.hidden, edges: .all)
             } header: {
@@ -97,49 +127,21 @@ extension PatientListScreen {
 struct PatientRow: View {
     let patientName: String
     
-    init(patientName: String) {
-        
-        self.patientName = patientName
-    }
-    
     var body: some View {
-        HStack {
-//            Text(patientName)
-//                .onTapGesture {
-//                    print("Go to details for: \(patientName)")
-//                }
-//                .padding(20)
-//                .background(Color.cyan)
-            
-            Button {
-                
-                
-            } label: {
-                Text(patientName)
-                    .foregroundStyle(.white)
-                    
-//                    .background(.cyan)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(8)
-            .background(.cyan)
-            .border(.green, width: 4)
-            .cornerRadius(5)
-            Spacer()
-            RoundedButton(title: "Screen", color: .cyan) {
-                print("Express screen: \(patientName)")
-            }
-        }
-        
-//        .border(.black)
-        
+        Text(patientName)
+        .foregroundStyle(.white)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(8)
+        .background(Color.blueTheme.background)
+//        .border(.green, width: 4)
+        .cornerRadius(5)
     }
 }
 
 
-//#Preview {
-//    PatientListScreen(vm: PatientListViewModel(loader: LocalPatientListLoader(), coordinator: CoordinatorObject(visitLoader: LocalPatientVisitsLoader())))
-//}
+#Preview {
+    PatientListScreen(vm: PatientListViewModel(loader: LocalPatientListLoader()))
+}
 
 //#Preview(traits: .sizeThatFitsLayout) {
 //    PatientRow(patientName: "Sammy Longjohn")
