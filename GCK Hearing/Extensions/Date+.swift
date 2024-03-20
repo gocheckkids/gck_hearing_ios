69d6036ec59f12c1564d33de9227a21545880f16
sharrarwasit <sharrar.wasit@gocheckkids.com>
2024-03-19 20:30:29 -0400

    added Date extensions

--- GCK Hearing/Extensions/Date+.swift
+++ GCK Hearing/Extensions/Date+.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 
+
 extension Date {
-    init(year: Int, month: Int, day: Int) {
+    
+    // Easier init with values
+     init(year: Int, month: Int, day: Int) {
         let calendar = Calendar.current
         var dateComponents = DateComponents()
         dateComponents.year = year
@@ -19,9 +22,19 @@ extension Date {
         
     }
     
+    // Return random date
     static func randomDate() -> Date {
          let randomInterval = TimeInterval(arc4random_uniform(UInt32.max))
          let randomDate = Date(timeIntervalSince1970: randomInterval)
          return randomDate
      }
+    
+    /// Compare date to today's date (omitting time)
+    func isCurrentDay() -> Bool {
+        return self.formattedAsDateString() == Date.now.formattedAsDateString()
+    }
+    /// Returns String as MM/DD/YYYY
+    func formattedAsDateString() -> String {
+        return self.formatted(date: .numeric, time: .omitted)
+    }
 }
